8f5f7e11bc14b6e9a4a290aa72424ad382213864
Vương Nguyễn <vuongnqv@gmail.com>
2021-12-22 09:04:21 +0700

    add file

---
+++ src/bubblesort/Concept.java
@@ -0,0 +1,9 @@
+package bubblesort;
+
+public class Concept {
+    /*
+    - Trong khoa học máy tính và trong toán học, thuật toán sắp xếp là một thuật toán sắp xếp các phần tử của một danh sách (hoặc một mảng) theo thứ tự (tăng hoặc giảm). Người ta thường xét trường hợp các phần tử cần sắp xếp là các số.
+    - Phân loại: Sắp xếp ổn định, sắp xếp so sánh
+    - Các thuật toán sắp xếp: Nổi bọt, chèn, chọn, trộn, vun đống, nhanh , theo cơ số , đếm phân phối, Shell sort
+     */
+}
